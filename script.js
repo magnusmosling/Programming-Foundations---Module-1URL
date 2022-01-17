a65957717bb5f17ac88d0235229e1e2f06f5ef93
Magnus <magnusmosling@live.no>
2022-01-17 11:27:32 +0100

    working on

---
+++ script.js
@@ -0,0 +1,42 @@
+//module Assignment 1
+
+//1.
+var firstvar = '';
+
+
+//2.
+var name = "magnus";
+
+
+//3.
+var number = 10;
+
+
+//4.
+var division = 20 / 5;
+console.log(division)
+
+
+//5.
+var animal = "frog";
+console.log (typeof animal)
+
+
+//6.
+var orderHasShipped = true;
+console.log(typeof orderHasShipped)
+
+
+//7.
+var orderHasShipped = true;
+if(orderHasShipped = true)
+console.log ("true");
+else
+console.log ("false");
+
+
+//8.
+
+for (var count = 1; count <=9; count = count + 1){
+    console.log (count)
+}
